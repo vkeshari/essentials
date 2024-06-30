504d5c19dcaba3fc948dbfede77236b3e91934a3
Vineet K <vineet.keshari@gmail.com>
2024-06-30 13:31:10 +0530

    Add numpy float range specifics

--- python/python_advanced.py
+++ python/python_advanced.py
@@ -6,15 +6,21 @@ sys.exit("Reference material. Do not execute.")
 import numpy as np
 
 np.average(data)
-np.percentile(date, 95)
+np.percentile(date, 95, method = 'nearest') # or use method = 'linear' for continuous
 
 # Floating point range by step
+np.arange(min_val, max_val, step) # excludes max_val like range
 np.arange(0, 2, 0.5) # --> [0.0, 0.5, 1.0, 1.5]
 np.arange(0.1, 1.5, 0.3) # --> [0.1, 0.4, 0.7, 1.0, 1.3]
 
 # Floating point range by bins
+np.linspace(min_val, max_val, bins) # includes max_val
 np.linspace(0, 10, 6) # --> [0, 2, 4, 6, 8, 10]
 np.linspace((0, 0), (10, 20), 3) # --> [[0, 0], [5, 10], [10, 20]]
+
+# Split a range without rounding errors
+bins = round((max_val - min_val) / step)
+np.linspace(min_val, max_val, bins)
 
 
 ### scipy.interpolate ###
